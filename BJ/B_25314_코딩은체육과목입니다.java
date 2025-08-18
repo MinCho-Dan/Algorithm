cbdc9b9d45733c429781aa296f7e447d0d053c87
gyujik <safre2@naver.com>
2025-08-18 22:29:51 +0900

    test

---
+++ BJ/B_25314_코딩은체육과목입니다.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B_25314_코딩은체육과목입니다 {
+
+	public static void main(String[] args) throws Exception {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int N = Integer.parseInt(br.readLine());
+	    int K = N/4;
+	    
+	    for (int i = 0; i < K-1; i++) {
+	    	System.out.print("long ");
+		}
+	    System.out.println("long int");
+
+        
+    }
+	
+}
